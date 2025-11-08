649b850dcd52aa7a2c19b4f472d04109ac60675f
rojko.alina <rojko.alina@gmail.com>
2025-11-08 20:03:18 +0000

    Implemented --browser_name fixture

--- playwright/conftest.py
+++ playwright/conftest.py
@@ -1,7 +1,24 @@
 import pytest
 
+def pytest_addoption(parser):
+    parser.addoption("--browser_name", action="store_true",
+                     default="chrome", help="browser selection")
 
 @pytest.fixture(scope="session")
 def user_credentials(request):
     return request.param
+
+@pytest.fixture
+def browserInstance(playwright, request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        browser = playwright.chromium.launch()
+    elif browser_name == "firefox":
+        browser = playwright.firefox.launch()
+
+    context = browser.new_context()
+    page = context.new_page()
+    yield page
+    context.close()
+    browser.close()
 
--- playwright/test_framwork_web_api.py
+++ playwright/test_framwork_web_api.py
@@ -4,33 +4,28 @@ from playwright.sync_api import Playwright, expect
 from pageObjects.loginPage import LoginPage
 from utils.apiBase import APIUtils
 
+# User_credentials extraction from .json file
 with open("playwright/data/credentials.json") as f:
     test_data = json.load(f)
-
 user_credentials_list = test_data["user_credentials"]
 
 @pytest.mark.parametrize('user_credentials', user_credentials_list)
-def test_e2e_web_api(playwright: Playwright, user_credentials):
-    browser = playwright.chromium.launch()
-    context = browser.new_context()
-    page = context.new_page()
+def test_e2e_web_api(playwright: Playwright, browser_name, user_credentials):
+    # User Credentials
+    user_email = user_credentials["user_email"]
+    user_password = user_credentials["user_password"]
 
     # Create order via API
     api_Utils = APIUtils()
     order_number = api_Utils.createOrder(playwright, user_credentials)
 
-    # User Credentials
-    user_email = user_credentials["user_email"]
-    user_password = user_credentials["user_password"]
-
     # Login
-    loginPage = LoginPage(page)
+    loginPage = LoginPage(browser_name)
     loginPage.navigate()
 
-    #Order history page
+    # Dashboard, Order history page
     dashBoardPage = loginPage.login(user_email, user_password)
     orderHistoryPage = dashBoardPage.selectOrdersNavigationLink()
     orderDetailsPage = orderHistoryPage.selectOrder(order_number)
     orderDetailsPage.verifyOrderMessage()
 
-    context.close()
